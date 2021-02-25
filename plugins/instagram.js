a4f477c3ce61c88ac090aeac46a290bebeec68b3
mabotsss <nandoscrz@gmail.com>
2021-02-25 19:35:27 +0700

    fix

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -1,13 +1,14 @@
 const Xscr = require('../events')
 const { MessageType } = require('@adiwajshing/baileys')
 const axios = require('axios')
+const got = require('got');
 
 const Language = require('../language')
 const { errorMessage, infoMessage } = require('../helpers')
 const Lang = Language.getString('instagram')
 
 Xscr.addCommand(
-  { pattern: 'insta ?(.*)', fromMe: true, usage: Lang.USAGE, desc: Lang.DESC },
+  { pattern: 'instagram ?(.*)', fromMe: true, usage: Lang.USAGE, desc: Lang.DESC },
   async (message, match) => {
     const userName = match[1]
 
@@ -49,3 +50,21 @@ Xscr.addCommand(
       )
   },
 )
+
+Xscr.addCommand({pattern: 'insta ?(.*)', fromMe: false, usage: Lang.USAGE, desc: Lang.DESC}, async (message, match) => {
+	if (match[1] === '') return await message.sendMessage(errorMessage(Lang.NEED_WORD));
+        /*await message.sendMessage(infoMessage(Lang.LOADING))*/
+	const url = `https://www.instagram.com/${match[1]}/?__a=1`;
+	try {
+		const response = await got(url);
+		const json = JSON.parse(response.body);
+		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*🏷' + Lang.USERNAME +':* ```' + match[1] + '```\n\n' +
+		'*🔖 ' + Lang.NAME +':* ```' + json.graphql.user.full_name + '```\n' + 
+		'*📄 ' + Lang.BIO +':* ```' + json.graphql.user.biography + '```\n' +
+		'*👣 ' + Lang.FOLLOWERS +':* ```' + json.graphql.user.edge_followed_by.count + '```\n' + 
+		'*👥 ' + Lang.FOLLOWS +':* ```' + json.graphql.user.edge_follow.count + '```\n' + 
+		'*🖼 ' + Lang.ACCOUNT +':* ```' + json.graphql.user.is_private + '```\n', MessageType.text);
+	} catch {
+		return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
+	}
+});
